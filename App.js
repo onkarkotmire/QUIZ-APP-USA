f067920c5ef43f5f3062fa4e77bd6e37b48cbf07
unknown <onkarkotmire2017@gmail.com>
2023-03-14 17:30:48 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Quiz from './components/QuizMain';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+      <h1>USA Quiz</h1>
+      <Quiz />
+      </>
+      
+    </div>
+  );
+}
+
+export default App;
